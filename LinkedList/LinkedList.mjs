77b2d5e7d404c6d2b7040d5972c7748141c4cec2
Rishabh Gour <rgour@paypal.com>
2021-06-25 14:41:54 +0530

    added more operations and refactor code

--- LinkedList/LinkedList.mjs
+++ LinkedList/LinkedList.mjs
@@ -1,16 +1,17 @@
 //* This module contains Linked List helper functions
 
 //returns a new node initialized with the arguments passed
-const node = (value, next = null) => ({value,next})
+const node = (value, next = null) => ({value, next})
 
 //returns the length of the list
-const length = (head, acc = 0) => !head ? acc : ( head.next ? length(head.next, acc + 1) : acc + 1 )
+const length = (head, acc = 0) => head.next ? length(head.next, acc + 1) : acc + 1
 
-//adds a node to the end of the list
-const append = (head, node) => !head ? node : (last(head).next = node)
-
-//returns reference to last node in the list
-const last = head => !head ? null : head.next ? last(head.next) : head
+//adds a node to the end of the list and returns head for chaining
+const append = (head, node) => {
+    if(!head) return node
+    _lastNode(head).next = node
+    return head
+}
 
 //adds a node to the start of the list and returns the new head
 const prepend = (head, node) => {
@@ -19,7 +20,39 @@ const prepend = (head, node) => {
     return node
 }
 
-//returns a string representation of all the elements of the list
-const print = head => !head ? '' : `${head.value} ${print(head.next)}`
+//deletes first node from the list and returns new head
+const deleteFirst = head => head.next ? head.next : null
 
-export default {node, length, append, last, prepend, print}
+//deletes last node from the list and returns head for chaining
+const deleteLast = head => head.next ? deleteLast(head.next) : null
+
+//returns the last value in the list
+const last = head => _lastNode(head).value
+
+//returns reference to node in the list
+const _lastNode = head => head.next ? _lastNode(head.next) : head
+
+//inserts a node on the given index and returns head for chaining
+const insert = (head, index, node) => {
+    if(index === 0) return prepend(head, node)
+    if(index >= length(head)) return append(head, node)
+    head.next = insert(head.next, index - 1, node)
+    return head
+}
+
+//returns a string representation of all the elements of the list
+const print = head => head.next ? `${head.value} ${print(head.next)}` : head.value
+
+export default 
+{
+    node, 
+    length, 
+    append, 
+    last, 
+    prepend, 
+    print, 
+    insert, 
+    deleteFirst,
+    deleteLast,
+    // delete
+}
--- LinkedList/test.mjs
+++ LinkedList/test.mjs
@@ -1,6 +1,6 @@
 import LinkedList from './LinkedList.mjs'
 
-const {node, length, append, last, prepend, print} = LinkedList
+const {node, length, append, last, prepend, print, insert} = LinkedList
 
 let head = node(1, null)
 append(head, node(2, null))
@@ -9,9 +9,11 @@ append(head, node(4, null))
 append(head, node(5, null))
 
 console.log( length(head) ) //prints: 5
-console.log( last(head) ) //prints: {value: 5, next: null}
+console.log( last(head) ) //prints: 5
 
 head = prepend(head, node(6, null))
-console.log(length(head))
-
+console.log(length(head)) //prints: 6
 console.log(print(head)) //prints: 6 1 2 3 4 5 
+
+head = insert(head, 2, node(8, null))
+console.log(print(head)) //prints: 6 1 8 2 3 4 5
